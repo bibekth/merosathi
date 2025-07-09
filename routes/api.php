8dce01eb852418295c3ee65d70335f4c3385cbda
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2025-07-09 14:36:09 +0000

    changes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,4 +8,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/gitlab-webhooks', [UserController::class, 'gitlabWebhook']);
+Route::post('/gitlab-webhooks', [ApiController::class, 'gitlabWebhook']);
