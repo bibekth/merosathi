6a867ca11e71d2229e49704f30fc2ce1d3d2d263
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2025-07-13 07:47:01 +0000

    changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,8 @@
     <link href="https://fonts.bunny.net/css?family=Montserrat" rel="stylesheet">
 
     <!-- Scripts -->
+    <link rel="stylesheet" href="{{ asset('build/assets/app-PUKBfmiD.css') }}">
+    <script src="{{ asset('build/assets/app-BDAque31.js') }}"></script>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script> --}}
