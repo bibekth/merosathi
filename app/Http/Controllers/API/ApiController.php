a711e922deb65d392880de95457c7fa1dab0c4ff
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2025-08-23 12:57:48 +0000

    changes

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -56,7 +56,7 @@ class ApiController extends BaseController
             $person->expected_date = $endDate->format('Y-m-d');
             $person->save();
 
-            return response()->json($endDate->format('Y-m-d'), 200);
+            return response()->json(['deliver date' => $endDate->format('Y-m-d')], 200);
             // return $this->sendResponse(['deliver date' => $endDate->format('Y-m-d')]);
         } catch (Throwable $e) {
             return $this->sendError($e->getMessage(), null, 500);
